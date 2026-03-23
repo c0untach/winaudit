28e77f5ee9f0b959d54e18058151f89e38101505
c0untach <51246376+c0untach@users.noreply.github.com>
2026-03-23 22:20:56 +0200

    Create Test-IsAdmin.ps1

---
+++ src/Private/Test-IsAdmin.ps1
@@ -0,0 +1,8 @@
+function Test-IsAdmin {
+    [OutputType([bool])]
+    param()
+
+    $current = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = New-Object Security.Principal.WindowsPrincipal($current)
+    return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
